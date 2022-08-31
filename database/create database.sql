c35f46d9c14c181b0529d910ea03378956278578
195152 <78798047+195152@users.noreply.github.com>
2022-08-31 16:24:53 +0200

    Add files via upload

---
+++ database/create database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `shopping` (
+    `id_item` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NOT NULL,
+    `quantity` VARCHAR(45) NOT NULL,
+    `purchased` TINYINT(1) NULL,
+)
+
